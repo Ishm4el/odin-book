54d95809a78abb853375824d97770c5599dcf3de
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-10-16 23:18:08 -0700

    Added comments to be displayed on the page.

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -6,99 +6,125 @@ import { database } from "~/database/context";
 import * as schema from "~/database/schema";
 import { and, asc, eq, getTableColumns, sql } from "drizzle-orm";
 
+export function meta({ params }: Route.MetaArgs) {
+  return [
+    { title: `Post: ${params.postId}` },
+    { name: "description", content: "Viewing a specfic post" },
+  ];
+}
+
 export async function loader({ request, params }: Route.LoaderArgs) {
   const user = await authenticate(request);
   const postId = params.postId;
 
   const db = database();
 
-  const { password, created, email, ...restOfUser } = getTableColumns(
-    schema.users
-  );
-
-  console.log("======================");
-
   try {
-    const thePost = await db
-      .select({
-        ...getTableColumns(schema.posts),
-        userHasLiked: sql<boolean>`EXISTS((${db
-          .select()
-          .from(schema.usersLikedPosts)
-          .where(
-            and(
-              eq(schema.usersLikedPosts.postId, schema.posts.id),
+    const [userLiked, postData] = await Promise.all([
+      db
+        .select({
+          userLiked: sql<boolean | undefined>`EXISTS((${db
+            .select()
+            .from(schema.usersLikedPosts)
+            .where(
               and(
-                eq(schema.usersLikedPosts.userId, user.id),
-                eq(schema.usersLikedPosts.like, true)
-              )
-            )
-          )}))`,
-        comments: sql<Array<typeof schema.comments.$inferSelect>>`
-        COALESCE(
-        array_agg(
-          json_build_object(
-            'id', ${schema.comments.id},
-            'content', ${schema.comments.text},
-            'createdAt', ${schema.comments.datePublished}
-          )
-        ) FILTER (WHERE ${schema.comments.id} IS NOT NULL),
-        '{}'::json[]
-      )
-    `,
-        author: sql<
-          Pick<
-            typeof schema.users.$inferSelect,
-            "id" | "firstName" | "lastName"
-          >
-        >`
-        (
-            SELECT json_build_object(
-                'userId', ${schema.users.id},
-                'firstName', ${schema.users.firstName},
-                'lastName', ${schema.users.lastName}
+                eq(schema.usersLikedPosts.postId, postId),
+                and(
+                  eq(schema.usersLikedPosts.userId, user.id),
+                  eq(schema.usersLikedPosts.like, true)
                 )
-            FROM users
-            WHERE users.id = ${schema.posts.authorId}
-        )
-      `,
-      })
-      .from(schema.posts)
-      .where(eq(schema.posts.id, postId))
-      .leftJoin(schema.comments, eq(schema.posts.id, schema.comments.postId))
-      .groupBy(schema.posts.id);
+              )
+            )}))`,
+        })
+        .from(schema.usersLikedPosts)
+        .where(eq(schema.usersLikedPosts.postId, postId)),
 
-    return thePost[0];
+      db.query.posts.findFirst({
+        where: eq(schema.posts.id, postId),
+        with: {
+          authorId: {
+            columns: {
+              id: true,
+              firstName: true,
+              lastName: true,
+              profilePictureAddress: true,
+            },
+          },
+          comments: {
+            with: {
+              authorId: {
+                columns: {
+                  id: true,
+                  firstName: true,
+                  lastName: true,
+                  profilePictureAddress: true,
+                },
+              },
+            },
+          },
+        },
+      }),
+    ]);
+
+    return { userLiked: userLiked[0], post: postData };
   } catch (error) {
     throw new Error(JSON.stringify(error));
   }
 }
 
+export function HydrateFallback() {
+  return <p>Loading Post!</p>;
+}
+
 export default function post({ loaderData }: Route.ComponentProps) {
-  return (
-    <article className="h-full bg-amber-50/20 w-full flex flex-col justify-between">
-      <section id="post-content" className="w-full">
-        <div id="post-header" className="bg-amber-100/99">
-          <h1 className="text-4xl p-2">{loaderData.title}</h1>
-          <h2 className="p-2 text-xl">
-            {loaderData.author.lastName}, {loaderData.author.firstName}
-          </h2>
-        </div>
-        <div id="text-content" className="bg-white border-amber-100 border p-4">
-          {loaderData.text}
-        </div>
-      </section>
-      <section id="post-comments" className="h-10 bg-white">
-        <ul>
-          {/* {loaderData.comments.map((comment) => (
-            <li>
-              <div>
-                <h2>{comment.}</h2>
-              </div>
-            </li>
-          ))} */}
-        </ul>
-      </section>
-    </article>
-  );
+  if (loaderData.post)
+    return (
+      <article className="h-full bg-amber-50/20 w-full flex flex-col justify-between">
+        <section id="post-content" className="w-full">
+          <div id="post-header" className="bg-amber-100/99">
+            <h1 className="text-4xl p-2">{loaderData.post.title}</h1>
+            <h2 className="p-2 text-xl">
+              {loaderData.post.authorId.lastName},{" "}
+              {loaderData.post.authorId.firstName}
+            </h2>
+          </div>
+          <div
+            id="text-content"
+            className="bg-white border-amber-100 border p-4"
+          >
+            {loaderData.post.text}
+          </div>
+        </section>
+        <section id="post-comments" className="bg-white">
+          <Form method="post" className="shadow mb-3 p-1 flex flex-col gap-3">
+            <div className="flex flex-col">
+              <label htmlFor="newComment" className="text-center mb-1 underline">
+                Comment?
+              </label>
+              <textarea name="newComment" id="newComment" className="ring"></textarea>
+            </div>
+            <button
+              type="submit"
+              className="border rounded mb-1 w-1/2 self-center hover:cursor-pointer bg-orange-50 hover:bg-orange-100 focus:bg-orange-100"
+            >
+              Post Comment
+            </button>
+          </Form>
+          <ul>
+            {loaderData.post.comments.map((comment) => (
+              <li className="mb-1 p-1 shadow">
+                <div className="flex gap-3 bg-amber-50/90">
+                  <h2>
+                    {comment.authorId.firstName} {comment.authorId.lastName}
+                  </h2>
+                  <h3>{comment.datePublished.toString()}</h3>
+                </div>
+                <div>{comment.text}</div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </article>
+    );
+  else return <article className="block">No post data was found..</article>;
 }
