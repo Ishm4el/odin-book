711d33dcde1a6bfe5d2208a31fa334aa851880d5
Ismael Valenzuela <ismaelv@protonmail.com>
2025-11-12 20:36:56 -0800

    Styled the search user page.

--- app/routes/users/index.tsx
+++ app/routes/users/index.tsx
@@ -2,13 +2,28 @@ import { Form } from "react-router";
 import type { Route } from "./+types/index";
 export default function Component({}) {
   return (
-    <section className="h-full w-full bg-white">
-      <h1>User Search</h1>
+    <section className="h-[50%] w-[50%] bg-amber-50">
       <search role="search">
-        <Form method="GET">
-          <label htmlFor="search-user">Search User:</label>
-          <input type="text" name="search-user" id="search-user" />
-          <button type="submit">Search</button>
+        <Form method="GET" className="flex flex-col items-center gap-3 p-5">
+          <label
+            htmlFor="search-user"
+            className="text-shadow text-center text-3xl text-amber-500"
+          >
+            Search User:
+          </label>
+          <input
+            placeholder="Enter a username"
+            type="text"
+            name="search-user"
+            id="search-user"
+            className="border bg-slate-100 p-1 hover:bg-slate-50 focus:bg-white active:bg-sky-50"
+          />
+          <button
+            type="submit"
+            className="rounded-full border bg-white p-2 px-5 text-slate-900 transition-colors hover:cursor-pointer hover:bg-sky-50 hover:text-black active:bg-sky-100"
+          >
+            Search
+          </button>
         </Form>
       </search>
     </section>
